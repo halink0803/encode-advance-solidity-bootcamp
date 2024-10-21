7233bcedf85c5851cdbab4b77ce37879148c5922
halink0803 <halink0803@gmail.com>
2024-10-21 10:58:12 +0700

    remove unused code

--- GasOptimisationFoundry/README.md
+++ GasOptimisationFoundry/README.md
@@ -17,3 +17,37 @@ Run: `forge test --gas-report`
 ## To run a specific test
 RUN:`forge test --match-test {TESTNAME} -vvvv`
 EG: `forge test --match-test test_onlyOwner -vvvv`
+
+## Current test result
+Suite result: ok. 12 passed; 0 failed; 0 skipped; finished in 1.26s (2.76s CPU time)
+| src/Gas.sol:GasContract contract |                 |        |        |        |         |
+|----------------------------------|-----------------|--------|--------|--------|---------|
+| Deployment Cost                  | Deployment Size |        |        |        |         |
+| 2659266                          | 11839           |        |        |        |         |
+| Function Name                    | min             | avg    | median | max    | # calls |
+| addToWhitelist                   | 34471           | 66109  | 84498  | 84726  | 2048    |
+| administrators                   | 2523            | 2523   | 2523   | 2523   | 5       |
+| balanceOf                        | 616             | 2044   | 2616   | 2616   | 1793    |
+| balances                         | 554             | 1054   | 554    | 2554   | 1024    |
+| checkForAdmin                    | 11621           | 11621  | 11621  | 11621  | 1       |
+| getPaymentStatus                 | 780             | 780    | 780    | 780    | 256     |
+| transfer                         | 174855          | 214979 | 217455 | 217551 | 1024    |
+| whiteTransfer                    | 105200          | 105399 | 105452 | 105464 | 768     |
+| whitelist                        | 598             | 598    | 598    | 598    | 512     |
+
+## Optimized test result
+Suite result: ok. 12 passed; 0 failed; 0 skipped; finished in 1.05s (2.33s CPU time)
+| src/Gas.sol:GasContract contract |                 |        |        |        |         |
+|----------------------------------|-----------------|--------|--------|--------|---------|
+| Deployment Cost                  | Deployment Size |        |        |        |         |
+| 1538792                          | 6861            |        |        |        |         |
+| Function Name                    | min             | avg    | median | max    | # calls |
+| addToWhitelist                   | 23954           | 45883  | 54392  | 54620  | 2048    |
+| administrators                   | 2501            | 2501   | 2501   | 2501   | 5       |
+| balanceOf                        | 604             | 2032   | 2604   | 2604   | 1793    |
+| balances                         | 587             | 1087   | 587    | 2587   | 1024    |
+| checkForAdmin                    | 11660           | 11660  | 11660  | 11660  | 1       |
+| getPaymentStatus                 | 723             | 723    | 723    | 723    | 256     |
+| transfer                         | 149617          | 189483 | 192229 | 192325 | 1024    |
+| whiteTransfer                    | 105014          | 105212 | 105278 | 105278 | 768     |
+| whitelist                        | 586             | 586    | 586    | 586    | 512     |
--- GasOptimisationFoundry/src/Gas.sol
+++ GasOptimisationFoundry/src/Gas.sol
@@ -16,44 +16,15 @@ contract GasContract is Ownable {
     mapping(address => uint256) public whitelist;
     mapping(address => uint256) public balances;
     address[5] public administrators;
-    uint256 private totalSupply; // cannot be updated
-    uint256 private paymentCounter;
-    address private contractOwner;
-    mapping(address => Payment[]) private payments;
+    uint256 private immutable totalSupply; // cannot be updated
+    address private immutable contractOwner;
     enum PaymentType {
         BasicPayment,
         Refund,
         Dividend,
         GroupPayment
-    }
-
-    History[] private paymentHistory; // when a payment was updated
-
-    struct Payment {
-        PaymentType paymentType;
-        uint256 paymentID;
-        bool adminUpdated;
-        string recipientName; // max 8 characters
-        address recipient;
-        address admin; // administrators address
-        uint256 amount;
-    }
-
-    struct History {
-        uint256 lastUpdate;
-        address updatedBy;
-        uint256 blockNumber;
-    }
-    
-    struct ImportantStruct {
-        uint256 amount;
-        uint256 valueA; // max 3 digits
-        uint256 bigValue;
-        uint256 valueB; // max 3 digits
-        bool paymentStatus;
-        address sender;
-    }
-    mapping(address => ImportantStruct) private whiteListStruct;
+    } 
+    mapping(address => uint256) private whiteListStruct;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
 
@@ -99,55 +70,24 @@ contract GasContract is Ownable {
         }
     }
 
-    function getPaymentHistory()
-        public
-        view
-        returns (History[] memory paymentHistory_)
-    {
-        return paymentHistory;
-    }
 
     function checkForAdmin(address _user) public view returns (bool admin_) {
-        bool admin = false;
         for (uint256 i; i < administrators.length; i++) {
             if (administrators[i] == _user) {
-                admin = true;
+                admin_ = true;
             }
         }
-        return admin;
     }
 
     function balanceOf(address _user) public view returns (uint256 balance_) {
         return balances[_user];
     }
 
-    function addHistory(address _updateAddress) 
-        internal 
-        returns (bool status_, bool tradeMode_)
-    {
-        History memory history = History(block.timestamp, _updateAddress, block.number);
-        paymentHistory.push(history);
-        return (true, true);
-    }
-
-    function getPayments(address _user)
-        public
-        view
-        returns (Payment[] memory payments_)
-    {
-        assembly {
-            if iszero(_user) {
-                revert(0, 0)
-            }
-        }
-        return payments[_user];
-    }
-
     function transfer(
         address _recipient,
         uint256 _amount,
         string calldata _name
-    ) public returns (bool status_) {
+    ) public returns (bool) {
         address sender = msg.sender;
         if (balances[sender] < _amount) {
             revert NotEnoughBalance();
@@ -158,46 +98,7 @@ contract GasContract is Ownable {
         balances[sender] -= _amount;
         balances[_recipient] += _amount;
         emit Transfer(_recipient, _amount);
-        Payment memory payment = Payment(PaymentType.BasicPayment,  ++paymentCounter, false, _name, _recipient, address(0), _amount);
-        payments[sender].push(payment);
         return true;
-    }
-
-    function updatePayment(
-        address _user,
-        uint256 _ID,
-        uint256 _amount,
-        PaymentType _type
-    ) public onlyAdminOrOwner {
-        assembly {
-            if iszero(_ID) {
-                revert(0, 0)
-            }
-            if iszero(_amount) {
-                revert(0, 0)
-            }
-            if iszero(_user) {
-                revert(0, 0)
-            }
-        }
-
-        address sender = msg.sender;
-
-        for (uint256 i; i < payments[_user].length; i++) {
-            if (payments[_user][i].paymentID == _ID) {
-                payments[_user][i].adminUpdated = true;
-                payments[_user][i].admin = _user;
-                payments[_user][i].paymentType = _type;
-                payments[_user][i].amount = _amount;
-                addHistory(_user);
-                emit PaymentUpdated(
-                    sender,
-                    _ID,
-                    _amount,
-                    payments[_user][i].recipientName
-                );
-            }
-        }
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier)
@@ -207,18 +108,7 @@ contract GasContract is Ownable {
         if (_tier >=255) {
             revert TierTooBig();
         }
-
-        whitelist[_userAddrs] = _tier;
-        if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
-        } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 2;
-        }
+        whitelist[_userAddrs] = _tier > 3 ? 3 : _tier;
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
@@ -227,7 +117,7 @@ contract GasContract is Ownable {
         uint256 _amount
     ) public checkIfWhiteListed(msg.sender) {
         address sender= msg.sender;
-        whiteListStruct[sender] = ImportantStruct(_amount, 0, 0, 0, true, msg.sender);
+        whiteListStruct[sender] = _amount;
         if ( balances[sender] < _amount ) {
             revert NotEnoughBalance();
         }
@@ -235,24 +125,14 @@ contract GasContract is Ownable {
             revert AmountTooSmall();
         }
         uint256 whitelistAmount = whitelist[sender];
-        balances[sender] -= _amount;
-        balances[_recipient] += _amount;
-        balances[sender] += whitelistAmount;
-        balances[_recipient] -= whitelistAmount;
+        balances[sender] = balances[sender] - _amount + whitelistAmount;
+        balances[_recipient] = balances[_recipient] + _amount - whitelistAmount;
         
         emit WhiteListTransfer(_recipient);
     }
 
     function getPaymentStatus(address sender) public view returns (bool, uint256) {
-        return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
+        return (true, whiteListStruct[sender]);
     }
 
-    receive() external payable {
-        payable(msg.sender).transfer(msg.value);
-    }
-
-
-    fallback() external payable {
-         payable(msg.sender).transfer(msg.value);
-    }
 }
